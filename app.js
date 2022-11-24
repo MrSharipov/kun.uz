fc3b18d4b6730494084454e734f8a730e7937c64
MrSharipov <84614549+MrSharipov@users.noreply.github.com>
2022-11-24 15:38:21 +0500

    Home page is ready

---
+++ app.js
@@ -0,0 +1,26 @@
+
+const apiLink = `https://newsapi.org/v2/top-headlines?country=us&apiKey=632e1ffdb365469d8ab34ba6447aafb2`
+
+const news_box = document.getElementById('news_box');
+
+const getNews = () => {
+    fetch(apiLink)
+        .then(res => res.json())
+        .then(data => showResult(data.articles));
+}
+
+const showResult = (data) => {
+    let text = '';
+    data.forEach(news => {
+    text += `<div class="news">
+                <h3>${news.title}</h3>
+                <img src=${news.urlToImage} alt="rasm" />
+                <p>${news.description}</p>
+            </div>
+        `
+    });
+    
+    news_box.innerHTML = text;
+}
+
+getNews();
